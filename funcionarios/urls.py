c3e1425a9affbbbdb9c93a7da451f01215e2ae8d
gustavo-it <gustavo_castrosilva@yahoo.com>
2023-01-25 11:19:18 -0300

    CRUD Funcionarios, Update

--- funcionarios/urls.py
+++ funcionarios/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import FuncionariosList
+from .views import FuncionariosList, FuncionarioEdit
 
 app_name = 'funcionarios'
 
 urlpatterns = [
     path('', FuncionariosList.as_view(), name='list'),
+    path('editar/<int:pk>/', FuncionarioEdit.as_view(), name='update')
 ]
--- funcionarios/views.py
+++ funcionarios/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView
 
 from .models import Funcionario
 
@@ -10,3 +10,7 @@ class FuncionariosList(ListView):
         empresa_logada = self.request.user.funcionario.empresa
         queryset = Funcionario.objects.filter(empresa=empresa_logada)
         return queryset
+    
+class FuncionarioEdit(UpdateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos',]
